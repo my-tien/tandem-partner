e54b99f529cc02bb7ae147a8186ea11eb1b75c74
My-Tien Nguyen <my-tien@mails.taipei>
2024-01-28 21:59:05 +0100

    API skeleton for choosing topic, sending message and seeing it in the history

--- backend/server/tandem.py
+++ backend/server/tandem.py
@@ -1,7 +1,7 @@
 from flask import Flask, render_template
-from flask import request
+from flask import request, redirect
 from flask_htmx import HTMX
-
+from datetime import datetime
 
 app = Flask(__name__)
 htmx = HTMX(app)
@@ -11,7 +11,13 @@ def home():
     return render_template("index.html")
 
 
-@app.route("/topic", methods=['GET', 'POST'])
-def topic():
-    print("request body", request.form)
-    return render_template("topic.html", context={"topic": request.form["topic"]})
+@app.route("/chat", methods=['GET', 'POST'])
+def chat():
+    message = request.form.get("message")
+    if message:
+        return render_template("message.html", name="Student", message=message, time=datetime.now().strftime("%H:%M:%S"))
+    topic = request.args.get("topic")
+    if topic:
+        return render_template("chat.html", topic=topic)
+    return redirect("/")
+    
