79af2647b50d00218ce16e20723d4bae1e6c645e
My-Tien Nguyen <my-tien@mails.taipei>
2024-01-28 10:42:58 +0100

    wip traditional character converter for document retriever

--- tandem/char_retrieval.py
+++ tandem/char_retrieval.py
@@ -6,6 +6,7 @@ from langchain.prompts import ChatPromptTemplate
 from langchain.chains import create_retrieval_chain
 from langchain.chains.combine_documents import create_stuff_documents_chain
 # from langchain.text_splitter import RecursiveCharacterTextSplitter
+from langchain_core.output_parsers import StrOutputParser
 from langchain.vectorstores.chroma import Chroma
 
 
@@ -22,23 +23,49 @@ def generate_character_db(character_txt_path: str, persist_directory: Optional[s
     return db
 
 
+def get_output_converter():
+    llm = ChatOpenAI(model_name="gpt-3.5-turbo", temperature=0)
+    converter_system_prompt = """Given the provided input, replace all simplified Chinese characters with traditional Chinese characters, add Pinyin and English translation. Do NOT answer the question, just return the input with the described changes.
+
+Output format:
+
+1. [chinese characters] (pinyin) – (english translation)
+...
+"""
+    converter_prompt = ChatPromptTemplate.from_messages(
+        [
+            ("system", converter_system_prompt),
+            ("user", "{input}"),
+        ]
+    )
+    converter_chain = converter_prompt | llm | StrOutputParser()
+    return converter_chain
+
 def get_character_list(db_path: str, topic):
     db = Chroma(persist_directory=db_path, embedding_function=OpenAIEmbeddings())
 
     prompt = ChatPromptTemplate.from_template(
 """
-Please select up to 10 characters from the provided context that could be used in a conversation about the provided topic.
+Please select up to 10 traditional characters from the provided context and form words with them that could be used in a Chinese conversation about the provided topic.
 
 <context>
 {context}
 </context>
 
 Topic: {input}
+
+Output format:
+
+1. [chinese characters]
+...
 """
     )
-
-    document_chain = create_stuff_documents_chain(llm=ChatOpenAI(), prompt=prompt)
+    document_chain = create_stuff_documents_chain(llm=ChatOpenAI(temperature=0.5), prompt=prompt)
     retriever = db.as_retriever()
-    retrieval_chain = create_retrieval_chain(retriever, document_chain)
-    result = retrieval_chain.invoke({"input": topic})
-    return result["answer"]
+    retrieval_chain = create_retrieval_chain(retriever, document_chain) | StrOutputParser()
+
+    converter_chain = get_output_converter()
+
+    chain = {"input": retrieval_chain} | converter_chain
+    result = chain.invoke({"input": topic})
+    return result
