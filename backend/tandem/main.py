7309f4204841c18bd6c4ae7f485942fc560a0ec4
My-Tien Nguyen <my-tien@mails.taipei>
2024-01-28 10:41:08 +0100

    Force traditional characters by using extra chain with model temperature=0

--- backend/tandem/main.py
+++ backend/tandem/main.py
@@ -7,28 +7,41 @@ from langchain_core.output_parsers import StrOutputParser
 from langchain_core.prompts import ChatPromptTemplate
 from tandem.char_retrieval import get_character_list
 
-def get_tandem_partner(character_list):
-    system_message = f"""Tandem language learning is an approach to language acquisition that is based on conversation between tandem partners. In this method, one partner serves as a native speaker of the language the other partner intends to learn.
-You are Lang, a tandem partner who is native in Chinese and who uses traditional Chinese Characters in writing.
-Your tandem partner intends to learn Chinese and traditional Chinese characters. She can already form very simple sentences (e.g. greeting, asking for directions, exressing ones mood). Your tandem partner can also understand Pinyin.
-In the provided context is a list of characters that your tandem partner intends to learn.
-You will use this knowledge about your tandem partner to make light conversation in Chinese that incorporates characters from the list. By doing that you will teach her typical Chinese expressions and how to incorporate the characters into conversational Chinese.
-It is crucial that you not only answer your partner's questions but also ask her questions back or make remarks, just like in a normal conversation between friends.
-When your tandem partner struggles to understand your sentence or doesn't know how to pronunciate a character, you will be happy to help her out in English and by using Pinyin.
-Each tandem language learning session starts with you and your tandem partner agreeing on a topic to talk about.
+def get_simplified_traditional_converter_chain():
+    llm = ChatOpenAI(model_name="gpt-3.5-turbo", temperature=0)
+    converter_system_prompt = """Given the provided input, replace all simplified Chinese characters with traditional Chinese characters and add a Pinyin transcription as well as an English translation. Add newlines between the Chinese, the Pinyin transcription and the English translation. Do NOT answer the input, just return the input with the described changes."""
+    converter_prompt = ChatPromptTemplate.from_messages(
+        [
+            ("system", converter_system_prompt),
+            ("user", "{input}"),
+        ]
+    )
+    converter_chain = converter_prompt | llm | StrOutputParser()
+    return converter_chain
+
+def get_tandem_chain(character_list):
+    llm = ChatOpenAI(model_name="gpt-3.5-turbo", temperature=0.7)
+    tandem_system_message = f"""You are Lang, a tandem partner who is native in Chinese. The user intends to practice Chinese and the typical usage of characters through a casual conversation with you. In the provided context is a list of characters that your tandem partner intends to practice. Whenever it makes sense, incorporate one or more of the characters into your response. Also include a remark or question toward the user to continue the conversation. Keep your response within 1 - 3 sentences.
 
 <context>
 {character_list}
 </context>
 """
 
-    prompt = ChatPromptTemplate.from_messages([("system", system_message), ("user", "{input}")])
-    llm = ChatOpenAI()
-    output_parser = StrOutputParser()
-    chain = prompt | llm | output_parser
+    tandem_prompt = ChatPromptTemplate.from_messages([("system", tandem_system_message), ("user", "{input}")])
+    tandem_chain = tandem_prompt | llm | StrOutputParser()
 
-    return chain
-    
+    return tandem_chain
+
+
+def get_tandem_partner(character_list):
+    tandem = get_tandem_chain(character_list)
+    converter = get_simplified_traditional_converter_chain()
+
+    tandem_partner = {"input": tandem} | converter
+    return tandem_partner
+
+
 
 if __name__ == '__main__':
     load_dotenv()
