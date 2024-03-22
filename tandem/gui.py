535b906e02d068fbe8a0d7ad9c5977258490c564
My-Tien Nguyen <my-tien@mails.taipei>
2024-03-22 22:17:20 +0100

    Replace Web UI with Qt gui.
    It's just easier for now for running openai requests in the background.

---
+++ tandem/gui.py
@@ -0,0 +1,110 @@
+from datetime import datetime
+import signal
+import sys
+from typing import Optional
+
+from PySide6 import QtCore, QtWidgets
+from PySide6 import QtGui
+
+from tandem.char_retrieval_chain import get_character_list
+from tandem_partner import TandemPartner
+
+
+def _get_time():
+    return datetime.now().strftime("%H:%M:%S")
+
+
+class Separator(QtWidgets.QFrame):
+    def __init__(self, parent: Optional[QtWidgets.QWidget] = None):
+        super(Separator, self).__init__(parent)
+        self.setFrameShape(QtWidgets.QFrame.HLine)
+        self.setFrameShadow(QtWidgets.QFrame.Sunken)
+        self.setSizePolicy(QtWidgets.QSizePolicy.Expanding, QtWidgets.QSizePolicy.Maximum)
+
+
+class ChatWindow(QtWidgets.QMainWindow):
+    def __init__(self, tandem):
+        super(ChatWindow, self).__init__()
+
+        self.setWindowTitle("Conversation with Lang")
+        width = 1280
+        height = 720
+        self.setGeometry(0, 0, width, height)
+        screen = QtGui.QScreen().geometry()
+        self.move((screen.width() - width) // 2, (screen.height() - height) // 2)
+
+        self.tandem_partner = tandem
+
+        self.topic_label = QtWidgets.QLabel(self.tandem_partner.character_list)
+
+        self.chat_history_widget = QtWidgets.QTextEdit(self)
+        self.chat_history_widget.setReadOnly(True)
+
+        self.message_input = QtWidgets.QLineEdit()
+        self.message_input.setPlaceholderText("Type a message…")
+        self.send_button = QtWidgets.QPushButton("Send")
+
+        self.vlayout = QtWidgets.QVBoxLayout()
+        self.vlayout.addWidget(self.topic_label)
+        self.vlayout.addWidget(Separator())
+        self.vlayout.addWidget(self.chat_history_widget)
+        self.vlayout.addWidget(Separator())
+        self.vlayout.addWidget(self.message_input)
+        self.vlayout.addWidget(self.send_button)
+
+        central_widget = QtWidgets.QWidget()
+        central_widget.setLayout(self.vlayout)
+        self.setCentralWidget(central_widget)
+
+        self.message_input.textChanged.connect(self._message_input_changed)
+        self.send_button.clicked.connect(self._send_button_clicked)
+        self.tandem_partner.response_signal.connect(self._display_response)
+
+    def _message_input_changed(self, text):
+        self.send_button.setEnabled = len(text) > 0
+
+    def _display_message(self, name, message):
+        history = self.chat_history_widget.toPlainText()
+        history += f"""
+[{name}] {_get_time()}:
+{message}
+
+"""
+        self.chat_history_widget.setPlainText(history)
+
+    def _send_button_clicked(self):
+        message = self.message_input.text()
+        self.message_input.setText("")
+        self._display_message("Student", message)
+        self.tandem_partner.invoke(message)
+
+    @QtCore.Slot(str)
+    def _display_response(self, response: str):
+        self._display_message("Lang", response)
+
+
+def open_topic_dialog():
+    topic, ok = QtWidgets.QInputDialog.getText(
+        None,
+        "Choose a topic for conversation",
+        "topic"
+    )
+    return topic if ok else None
+
+
+if __name__ == '__main__':
+    app = QtWidgets.QApplication(sys.argv)
+    app.setApplicationName('Tandem Partner')
+
+    topic = open_topic_dialog()
+    if not topic:
+        QtCore.QCoreApplication.exit()
+
+    character_list = get_character_list(topic="traveling")
+    tandem = TandemPartner(character_list)
+
+    window = ChatWindow(tandem)
+    window.show()
+
+    signal.signal(signal.SIGINT, signal.SIG_DFL)
+    sys.exit(app.exec())
---
+++ tandem/tandem_partner.py
@@ -0,0 +1,42 @@
+from langchain.memory import ChatMessageHistory
+from PySide6.QtCore import QObject, QThread, Signal, Slot
+from tandem.conversation_chain import get_tandem_chain, get_simplified_traditional_converter_chain
+
+
+class ResponseWorker(QThread):
+    response_received = Signal(str)
+
+    def __init__(self, chain, message):
+        super(ResponseWorker, self).__init__()
+        self.chain = chain
+        self.message = message
+        
+    def run(self):
+        response = self.chain.invoke({"input": self.message})
+        self.response_received.emit(response)
+        
+
+class TandemPartner(QObject):
+    response_signal = Signal(str)
+
+    def __init__(self, character_list):
+        super(TandemPartner, self).__init__()
+        tandem_chain = get_tandem_chain(character_list)
+        converter_chain = get_simplified_traditional_converter_chain()
+
+        self.character_list = character_list
+        self.chain = {"input": tandem_chain} | converter_chain
+        self.worker = None
+        self.chat_history = ChatMessageHistory()
+    
+    def invoke(self, message: str):
+        self.chat_history.add_user_message(message)
+        self.worker = ResponseWorker(self.chain, self.chat_history)
+        self.worker.response_received.connect(self.handle_response)
+        self.worker.start()
+
+    @Slot(str)
+    def handle_response(self, response):
+        self.worker.response_received.disconnect(self.handle_response)
+        self.chat_history.add_ai_message(response)
+        self.response_signal.emit(response)
