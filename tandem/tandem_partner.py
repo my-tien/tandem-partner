e3b3626c5566675fb4afb1ccf6306062213a0d22
My-Tien Nguyen <my-tien@mails.taipei>
2024-06-15 23:11:07 +0200

    Add text2speech support to tandem_partner
    Adds message index to Response for referencing the message to play back.

--- tandem/tandem_partner.py
+++ tandem/tandem_partner.py
@@ -1,4 +1,7 @@
+from pathlib import Path
+
 from langchain.memory import ChatMessageHistory
+from openai import OpenAI
 from PySide6.QtCore import QObject, QThread, Signal, Slot
 from tandem.conversation_chain import get_tandem_chain, get_simplified_traditional_converter_chain
 
@@ -16,7 +19,8 @@ class ResponseWorker(QThread):
         self.response_received.emit(response)
         
 class Response:
-    def __init__(self, msg):
+    def __init__(self, msg, idx):
+        self.idx = idx
         self.traditional = ""
         self.pinyin = ""
         self.english = ""
@@ -40,15 +44,34 @@ class TandemPartner(QObject):
         self.chain = {"input": tandem_chain} | converter_chain
         self.worker = None
         self.chat_history = ChatMessageHistory()
+        self.history_length = 0
+        self.openai_client = OpenAI()
     
-    def invoke(self, message: str):
+    def _add_user_message(self, message):
         self.chat_history.add_user_message(message)
+        self.history_length += 1
+
+    def _add_ai_message(self, message) -> int:
+        self.chat_history.add_ai_message(message)
+        message_idx = self.history_length
+        self.history_length += 1
+        return message_idx
+
+    def invoke(self, message: str):
+        self._add_user_message(message)
         self.worker = ResponseWorker(self.chain, self.chat_history)
         self.worker.response_received.connect(self.handle_response)
         self.worker.start()
 
     @Slot(str)
-    def handle_response(self, response):
+    def handle_response(self, response: str):
         self.worker.response_received.disconnect(self.handle_response)
-        self.chat_history.add_ai_message(response)
-        self.response_signal.emit(response)
+        idx = self._add_ai_message(response)
+        self.response_signal.emit(Response(response, idx))
+
+    def text2speech(self, history_index: int, output_path: str):
+        if not Path(output_path).exists():
+            message = self.chat_history.messages[history_index]
+            chinese = Response(message.content, history_index).traditional
+            response = self.openai_client.audio.speech.create(model="tts-1", voice="echo", input=chinese, response_format="mp3")
+            response.stream_to_file(output_path)
